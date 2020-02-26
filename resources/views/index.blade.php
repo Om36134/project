3dd2bfa7aece9891f966c1f3e3671baa683d63c2
Oksana Morozova <om36134@gmail.com>
2020-02-26 13:15:00 +0300

    project end

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,17 +20,15 @@
 
     {{--Font Style--}}
 
-     <link rel="stylesheet" href="/css/fonts/league-spartan.css">
+    <link rel="stylesheet" href="/css/fonts/league-spartan.css">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700|Poppins:300&display=swap"
           rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Poppins:300&display=swap" rel="stylesheet">
 
-{{--Owl Carusel--}}
+    {{--Owl Carusel--}}
     <link rel="stylesheet" href="./libs/owlcarusel2/assets/owl.carousel.min.css">
     <link rel="stylesheet" href="./libs/owlcarusel2/assets/owl.theme.default.min.css">
-
-
 
 
 </head>
@@ -43,74 +41,74 @@
 
 
 
-    {{--@include('block.navigation')--}}
-    {{--@include('block.slider')--}}
+@include('block.navigation')
+@include('block.slider')
 
-{{--<section class="quote">--}}
-    {{--<div class="quote-wrapper">--}}
-        {{--<div class="quote__text">--}}
-            {{--Surfing is the most blissful--}}
-            {{--experience you can have on this planet, a--}}
-            {{--taste of heaven.--}}
-        {{--</div>--}}
-        {{--<div class="quote__author">John McCarthy</div>--}}
-    {{--</div>--}}
-{{--</section>--}}
+<section class="quote">
+    <div class="quote-wrapper">
+        <div class="quote__text">
+            Surfing is the most blissful
+            experience you can have on this planet, a
+            taste of heaven.
+        </div>
+        <div class="quote__author">John McCarthy</div>
+    </div>
+</section>
 
-{{--<section class="club-description ">--}}
+<section class="club-description ">
 
-    {{--<div class="container">--}}
+    <div class="container">
 
-        {{--<div class="club-description-wrapper">--}}
+        <div class="club-description-wrapper">
 
-            {{--<div class="club-description__img">--}}
+            <div class="club-description__img">
 
-                {{--<img src="img/img/img-2.jpg" alt="Picture">--}}
+                <img src="img/img/img-2.jpg" alt="Picture">
 
-            {{--</div>--}}
+            </div>
 
-            {{--<div class="club-description-video">--}}
+            <div class="club-description-video">
 
-                {{--<img src="img/img/img-3.jpg" alt="Video">--}}
+                <img src="img/img/img-3.jpg" alt="Video">
 
-                {{--<div class="club-description-video__control">--}}
+                <div class="club-description-video__control">
 
-                    {{--<img src="img/controls/control-play.svg" alt="Video">--}}
+                    <img src="img/controls/control-play.svg" alt="Video">
 
-                {{--</div>--}}
+                </div>
 
-            {{--</div>--}}
+            </div>
 
-            {{--<div class="club-description__text">--}}
+            <div class="club-description__text">
 
-                {{--<p>By better understanding--}}
-                    {{--the various aspects of surfing, you wil improve faster--}}
-                    {{--and have more fun in the water.</p>--}}
-            {{--</div>--}}
+                <p>By better understanding
+                    the various aspects of surfing, you wil improve faster
+                    and have more fun in the water.</p>
+            </div>
 
-            {{--<div class="club-description__link">--}}
-                {{--<a class="read-more-link" href="#"> Read More</a>--}}
-            {{--</div>--}}
+            <div class="club-description__link">
+                <a class="read-more-link" href="#"> Read More</a>
+            </div>
 
-        {{--</div>--}}
+        </div>
 
-    {{--</div>--}}
+    </div>
 
-{{--</section>--}}
+</section>
 
-{{--@include('block.shop')--}}
-{{--@include('block.article-01')--}}
-{{--@include('block.article-02')--}}
-{{--@include('block.subscribe')--}}
+@include('block.shop')
+@include('block.article-01')
+@include('block.article-02')
+@include('block.subscribe')
 @include('block.camp')
-{{--@include('block.footer')--}}
+@include('block.footer')
 
 
 
 {{--<!-- Latest compiled and minified JavaScript -->--}}
 {{--<script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"--}}
-        {{--integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"--}}
-        {{--crossorigin="anonymous"></script>--}}
+{{--integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"--}}
+{{--crossorigin="anonymous"></script>--}}
 
 {{--Owl Carusel--}}
 
